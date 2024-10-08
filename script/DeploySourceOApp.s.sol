0609378cce9b379ab209f4329273c31a87dcac7c
Aayush Gupta <guptaaayush432@gmail.com>
2024-10-08 21:44:52 +0530

    updated solc version, added better comments

--- script/DeploySourceOApp.s.sol
+++ script/DeploySourceOApp.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
 import {SourceOApp} from "../src/SourceOApp.sol"; // Adjust this path to where your contract is located
@@ -17,7 +17,9 @@ contract DeploySourceOApp is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy the contract
-        SourceOApp sourceOApp = new SourceOApp(endpoint, owner);
+        // SourceOApp sourceOApp = new SourceOApp(endpoint, owner);
+
+        SourceOApp sourceOApp = new SourceOApp(endpoint);
 
         // Log the deployed contract address
         console.log("SourceOApp contract ARB SEPOLIA CHAIN deployed at:", address(sourceOApp));
@@ -30,4 +32,8 @@ contract DeploySourceOApp is Script {
 // arb sepolia endpoint : 0x6EDCE65403992e310A62460808c4b910D972f10f
 // Arb sepolia : 40231
 
+// deployed address : 0x8dd1414EA59D6a7c64167f9F403df5F49C20538F
+
 // forge script script/DeploySourceOApp.s.sol:DeploySourceOApp --rpc-url $ARBITRUM_SEPOLIA_RPC_URL --broadcast -vvvv
+
+// forge script script/DeploySourceOApp.s.sol:DeploySourceOApp --rpc-url $ARBITRUM_SEPOLIA_RPC_URL --etherscan-api-key $ARBISCAN_API_KEY --chain 42162 --broadcast --verify -vvvv
