5fb28cfc508c4beae81cca23bc011ae3b771d12b
Aayush Gupta <guptaaayush432@gmail.com>
2024-10-08 21:44:47 +0530

    updated solc version, added better comments

--- script/DeployDestinationOApp.s.sol
+++ script/DeployDestinationOApp.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
-import {DestinationOApp} from "../src/DestinationOApp.sol";  // Adjust this path if your contract is located elsewhere
+import {DestinationOApp} from "../src/DestinationOApp.sol";  
 
 contract DeployDestinationOApp is Script {
     // Define the LayerZero endpoint address and owner address
@@ -29,5 +29,8 @@ contract DeployDestinationOApp is Script {
 
 // op sepolia endpoint: 0x6EDCE65403992e310A62460808c4b910D972f10f
 // op sepolia : 40232
+// Op sepolia Contract address : 0x8dd1414EA59D6a7c64167f9F403df5F49C20538F
 
 // forge script script/DeployDestinationOApp.s.sol:DeployDestinationOApp --rpc-url $OPTIMISM_SEPOLIA_RPC_URL --broadcast -vvvv
+
+// forge script script/DeployDestinationOApp.s.sol:DeployDestinationOApp --rpc-url $OPTIMISM_SEPOLIA_RPC_URL --etherscan-api-key $OPTIMISM_API_KEY --chain 11155420 --broadcast --verify -vvvv
